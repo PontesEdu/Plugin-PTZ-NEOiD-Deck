cc42725e3334a1addbd7846c552c7c39ef764ca7
PontesEdu <edupontessilva03@gmail.com>
2025-10-17 15:08:55 -0300

    I fixed the backlight that was not turning off on the camera

--- src/actions/backlight.ts
+++ src/actions/backlight.ts
@@ -2,83 +2,6 @@ import streamDeck, { action, DidReceiveSettingsEvent, KeyDownEvent, SingletonAct
 import { apiBasePtzPostImageValue } from "../utils/ptz-api-post-image-value";
 
 
-// Ações
-// @action({ UUID: "com.neoid.ptzneoid.backlight" })
-// export class Backlight extends SingletonAction {
-//   private isBacklight = false;
-
-//   override async onWillAppear(ev: WillAppearEvent) {
-//     const globals = await streamDeck.settings.getGlobalSettings();
-    
-//     const cameraIP = globals.cameraIP;
-
-//     if (!cameraIP) {
-//       ev.action.setTitle(`${globals.camera}`)
-//       return;
-//     }
-
-//     // Converte para booleano corretamente
-//     this.isBacklight = globals.isBacklight === true || globals.isBacklight === "true";
-
-//     await ev.action.setTitle(this.isBacklight ? "Backlight\nON" : "Backlight\nOFF");
-//   }
-
-//   override async onDidReceiveSettings(ev: DidReceiveSettingsEvent){
-//     const globals = await streamDeck.settings.getGlobalSettings();
-    
-//     const cameraIP = globals.cameraIP;
-
-//     if (!cameraIP) {
-//       ev.action.setTitle(`${globals.camera}`)
-//       return;
-//     }
-
-//     // Converte para booleano corretamente
-//     this.isBacklight = globals.isBacklight === true || globals.isBacklight === "true";
-
-//     await ev.action.setTitle(this.isBacklight ? "Backlight\nON" : "Backlight\nOFF");
-//   }
-
-//   override async onKeyDown(ev: KeyDownEvent): Promise<void> {
-//     const globals = await streamDeck.settings.getGlobalSettings();
-//     const cameraIP = globals.cameraIP;
-
-//     ev.action.setTitle(`${globals.camera}`)
-//     if (!cameraIP) {
-//       ev.action.setTitle(`${globals.camera}`)
-//       return;
-//     }
-
-//     this.isBacklight = !this.isBacklight;
-
-//     if (this.isBacklight) {
-//       const apiBase = apiBasePtzPostImageValue(cameraIP)
-
-//       const url = `${apiBase}&backlight&1`
-//       const response = await fetch(url);  
-
-//       const urlNew = `${apiBase}&backlight&2`
-//       await fetch(urlNew);  
-
-//       await ev.action.setTitle("Backlight\nON");
-//     } else { 
-//       const apiBase = apiBasePtzPostImageValue(cameraIP)
-//       let url = `${apiBase}&backlight&0`
-
-//       await fetch(url);
-      
-//       const urlNew = `${apiBase}&backlight&3`
-//       await fetch(urlNew);  
-
-//       await ev.action.setTitle("Backlight\nOFF");
-//     }
-
-//     await streamDeck.settings.setGlobalSettings({
-//       ...globals,
-//       isBacklight: this.isBacklight,
-//     });
-//   }
-// }
 
 
 @action({ UUID: "com.neoid.ptzneoid.backlight" })
@@ -110,11 +33,14 @@ export class Backlight extends SingletonAction {
 
   private async toggleBacklightAPI(cameraIP: string, enable: boolean) {
     const apiBase = apiBasePtzPostImageValue(cameraIP);
-    const urlMain = `${apiBase}&backlight&${enable ? 1 : 0}`;
-    const urlSecondary = `${apiBase}&backlight&${enable ? 2 : 3}`;
-
-    await fetch(urlMain);
-    await fetch(urlSecondary);
+    
+    try {
+      const urlMain = `${apiBase}&backlight&${enable ? 1 : 0}`;
+      await fetch(urlMain);
+    } catch (error) {
+      const urlSecondary = `${apiBase}&backlight&${enable ? 2 : 3}`;
+      await fetch(urlSecondary);
+    }
   }
 
   private async refreshState(ev: { action: any }) {
